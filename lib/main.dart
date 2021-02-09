60eef5fcb84aedcd19420b75c407ced83ba2681d
Lefted <gartnermoritz@yahoo.de>
2021-02-09 10:31:18 +0100

    Select meals for category and Fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/categories_screen.dart';
-import 'package:meals_app/category_meals_scree.dart';
+import 'package:meals_app/category_meals_screen.dart';
 
 void main() => runApp(MealsApp());
 
